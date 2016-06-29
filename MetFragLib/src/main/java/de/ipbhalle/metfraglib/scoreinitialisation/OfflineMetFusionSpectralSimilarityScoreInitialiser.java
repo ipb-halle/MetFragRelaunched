f419c49469eda3580d2b42ea51032934301f262e
Ruttkies <cruttkie@localhost>
2016-06-29 20:12:40 +0200

    increased session timeout

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/scoreinitialisation/OfflineMetFusionSpectralSimilarityScoreInitialiser.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/scoreinitialisation/OfflineMetFusionSpectralSimilarityScoreInitialiser.java
@@ -13,21 +13,27 @@ public class OfflineMetFusionSpectralSimilarityScoreInitialiser implements IScor
 	public void initScoreParameters(Settings settings) throws Exception {
 		String offlineSpectralFilePath = "";
 		java.io.InputStream is = null;
-		if(settings.containsKey(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) && settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) != null) {
-			offlineSpectralFilePath = (String)settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME);
-			is = new java.io.FileInputStream(offlineSpectralFilePath);
-		} else { 
-			is = OfflineMetFusionSpectralSimilarityScoreInitialiser.class.getResourceAsStream("/MoNA-export-LC-MS.mb");
+		try {
+			if(settings.containsKey(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) && settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) != null) {
+				offlineSpectralFilePath = (String)settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME);
+				is = new java.io.FileInputStream(offlineSpectralFilePath);
+			} else { 
+				is = OfflineMetFusionSpectralSimilarityScoreInitialiser.class.getResourceAsStream("/MoNA-export-LC-MS.mb");
+			}
+			//check whether MoNA InChIKeys are given as resource
+			MultipleTandemMassPeakListReader multiplePeakListReader = new MultipleTandemMassPeakListReader(settings);
+			SpectralPeakListCollection spectralPeakLists = null;
+			if(offlineSpectralFilePath.length() != 0) spectralPeakLists = multiplePeakListReader.readMultiple(offlineSpectralFilePath);
+			else spectralPeakLists = multiplePeakListReader.readMultiple(is);
+			spectralPeakLists.calculateSimilarities((SortedTandemMassPeakList)settings.get(VariableNames.PEAK_LIST_NAME));
+			is.close();
+			settings.set(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME, spectralPeakLists);
+		} catch(Exception e) {
+			throw new Exception();
 		}
-		//check whether MoNA InChIKeys are given as resource
-		MultipleTandemMassPeakListReader multiplePeakListReader = new MultipleTandemMassPeakListReader(settings);
-		SpectralPeakListCollection spectralPeakLists = null;
-		if(offlineSpectralFilePath.length() != 0) spectralPeakLists = multiplePeakListReader.readMultiple(offlineSpectralFilePath);
-		else spectralPeakLists = multiplePeakListReader.readMultiple(is);
-		spectralPeakLists.calculateSimilarities((SortedTandemMassPeakList)settings.get(VariableNames.PEAK_LIST_NAME));
-		is.close();
-	
-		settings.set(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME, spectralPeakLists);
+		finally {
+			if(is != null) is.close();
+		}
 	}
 
 }
