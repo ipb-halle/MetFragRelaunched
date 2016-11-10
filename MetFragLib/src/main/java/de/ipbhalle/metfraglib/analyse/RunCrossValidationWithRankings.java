3af49a5da10d40c79f7c9f48971cac7d101eca4e
Christoph Ruttkies <chrisr@troll>
2016-11-10 22:06:39 +0100

    update

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/analyse/RunCrossValidationWithRankings.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/analyse/RunCrossValidationWithRankings.java
@@ -14,15 +14,17 @@ import de.ipbhalle.metfraglib.additionals.MathTools;
 public class RunCrossValidationWithRankings {
 
 	//public static String rankings_folder_name = "/home/cruttkie/Dokumente/PhD/MetFrag/substructure_training/cross_validation/eawag_02_new/rankings_testing_combined";
-	public static String rankings_folder_name = "/home/cruttkie/svn/eawag/2016hdx/metfrag/rankings_9_chemspider/pos";
+	//public static String rankings_folder_name = "/home/cruttkie/svn/eawag/2016hdx/metfrag/rankings_9_chemspider/pos";
+	public static String rankings_folder_name = "/home/chrisr/Dokumente/PhD/Talks/FoSem2016/train_ufz_test_eawag_05_11_2016/rankings";
 	public static int number_folds = 10;
 	public static int number_queries = 1;
-	public static String only_metfrag_filename = "rankings_1001.txt";
+	public static String only_metfrag_filename = "rankings_1005.txt";
 	//public static String only_metfrag_filename = "rankings_101.txt";
 	//public static String given_folds_filename = "/home/cruttkie/Dokumente/PhD/MetFrag/substructure_training/cross_validation/eawag_01_new/folds.txt";
 	public static String given_folds_filename = null;
 	//public static String output_file = "/home/cruttkie/Dokumente/PhD/MetFrag/substructure_training/cross_validation/eawag_02_new/rankings.txt";
-	public static String output_file = "/home/cruttkie/svn/eawag/2016hdx/metfrag/rankings_9_chemspider/pos_rankings.txt";
+	//public static String output_file = "/home/cruttkie/svn/eawag/2016hdx/metfrag/rankings_9_chemspider/pos_rankings.txt";
+	public static String output_file = "/home/chrisr/Dokumente/PhD/Talks/FoSem2016/train_ufz_test_eawag_05_11_2016/rankings_cross.txt";
 	//public static String output_file = null;
 
 	
