c6c1f3255e6608453314e52056f18c7362b3b358
Ruttkies <cruttkie@localhost>
2017-01-16 15:15:19 +0100

    update

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/database/LocalMetChemDatabase.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/database/LocalMetChemDatabase.java
@@ -6,7 +6,6 @@ import java.util.Vector;
 
 import de.ipbhalle.metfraglib.additionals.MathTools;
 import de.ipbhalle.metfraglib.candidate.TopDownPrecursorCandidate;
-import de.ipbhalle.metfraglib.exceptions.DatabaseIdentifierNotFoundException;
 import de.ipbhalle.metfraglib.interfaces.ICandidate;
 import de.ipbhalle.metfraglib.list.CandidateList;
 import de.ipbhalle.metfraglib.parameter.VariableNames;
@@ -15,6 +14,8 @@ import de.ipbhalle.metfraglib.settings.Settings;
 public class LocalMetChemDatabase extends LocalPostgresDatabase {
 	
 	protected String library;
+	protected CandidateList tempCandidates;
+	protected Vector<String> tempSubstanceIDs = new Vector<String>();
 	
 	public LocalMetChemDatabase(Settings settings) {
 		super(settings);
@@ -33,183 +34,15 @@ public class LocalMetChemDatabase extends LocalPostgresDatabase {
 		this.INCHIKEY1_COLUMN_NAME		=	"inchi_key_1";
 		this.INCHIKEY2_COLUMN_NAME		=	"inchi_key_2";
 		this.CID_COLUMN_NAME			=	"accession";
-		//this.FORMULA_COLUMN_NAME		=	"molecular_formula";
-		//this.MASS_COLUMN_NAME			=	"monoisotopic_mass";
-		this.FORMULA_COLUMN_NAME		=	"formula";
-		this.MASS_COLUMN_NAME			=	"exact_mass";
+		this.FORMULA_COLUMN_NAME		=	"molecular_formula";
+		this.MASS_COLUMN_NAME			=	"monoisotopic_mass";
 		this.COMPOUND_NAME_COLUMN_NAME	=	"name";
 		
 	}
-
-	public Vector<String> getCandidateIdentifiers(double monoisotopicMass, double relativeMassDeviation) {
-		int library_id = this.getLibraryIdentifier();
-		if(library_id == -1) return null;
-		
-		double error = MathTools.calculateAbsoluteDeviation(monoisotopicMass, relativeMassDeviation);
-		
-		String query = "select substance." + this.CID_COLUMN_NAME + " from substance, compound" 
-				+ " where compound." + this.MASS_COLUMN_NAME + " between " + (monoisotopicMass - error) + " and " + (monoisotopicMass + error)
-				+ " and substance.compound_id=compound.compound_id and substance.library_id='" + library_id + "';"; 
-		
-		ResultSet rs = this.submitQuery(query);
-		Vector<String> cids = new Vector<String>();
-		if(rs == null) return cids;
-		try {
-			while(rs.next()) cids.add(rs.getString(this.CID_COLUMN_NAME));
-			rs.close();
-			this.statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return cids;
-	}
-
-	public Vector<String> getCandidateIdentifiers(String molecularFormula) {
-		int library_id = this.getLibraryIdentifier();
-		if(library_id == -1) return null;
-		
-		String query = "select substance." + this.CID_COLUMN_NAME + " from substance, compound" 
-				+ " where compound." + this.FORMULA_COLUMN_NAME + "='" + molecularFormula + "'" 
-				+ " and substance.compound_id=compound.compound_id and substance.library_id='" + library_id + "';"; 
-		
-		ResultSet rs = this.submitQuery(query);
-		Vector<String> cids = new Vector<String>();
-		if(rs == null) return cids;
-		try {
-			while(rs.next()) cids.add(rs.getString(this.CID_COLUMN_NAME));
-			rs.close();
-			this.statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return cids;
-	}
-
-	public Vector<String> getCandidateIdentifiers(Vector<String> identifiers) {
-		if(identifiers.size() == 0) return new Vector<String>();
-		
-		int library_id = this.getLibraryIdentifier();
-		if(library_id == -1) return null;
-		
-		String identifierString = identifiers.get(0);
-		for(String cid : identifiers)
-			identifierString += ",'" + cid + "'";
-		String query = "select " + this.CID_COLUMN_NAME + " from substance "
-				+ "where " + this.CID_COLUMN_NAME + " in (" + identifierString + ") and substance.library_id='" + library_id + "';"; 
-		
-		
-		
-		ResultSet rs = this.submitQuery(query);
-		Vector<String> cids = new Vector<String>();
-		if(rs == null) return cids;
-		try {
-			while(rs.next()) cids.add(rs.getString(this.CID_COLUMN_NAME));
-			rs.close();
-			this.statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return cids;
-	}
-
-	public ICandidate getCandidateByIdentifier(String identifier)
-			throws DatabaseIdentifierNotFoundException {
-					
-		int library_id = this.getLibraryIdentifier();
-		if(library_id == -1) return null;
-		String query = "select substance." + this.CID_COLUMN_NAME + " as " + this.CID_COLUMN_NAME + ", name." + this.COMPOUND_NAME_COLUMN_NAME 
-				+ " as " + this.COMPOUND_NAME_COLUMN_NAME + ", compound." + this.SMILES_COLUMN_NAME + " as " + this.SMILES_COLUMN_NAME 
-				+ ", compound." + this.INCHI_COLUMN_NAME + " as " + this.INCHI_COLUMN_NAME + ", compound." 
-				+ this.MASS_COLUMN_NAME + " as " + this.MASS_COLUMN_NAME + " ," + " compound." + this.FORMULA_COLUMN_NAME 
-				+ " as " + this.FORMULA_COLUMN_NAME + ", compound." + this.INCHIKEY1_COLUMN_NAME + " as " + this.INCHIKEY1_COLUMN_NAME 
-				+ ", compound." + this.INCHIKEY2_COLUMN_NAME + " as " + this.INCHIKEY2_COLUMN_NAME 
-				+ ", substance.compound_id, substance.substance_id from substance, compound, name" 
-				+ " where substance.library_id='" + library_id + "'" 
-				+ " and substance." + this.CID_COLUMN_NAME + " = '" + identifier + "' "
-				+ " and substance.compound_id=compound.compound_id" 
-				+ " and name.substance_id=substance.substance_id"; 		
-		
-		ResultSet rs = this.submitQuery(query);
-		if(rs == null) return null;
-		Vector<String> inchis = new Vector<String>();
-		Vector<String> inChIKeys1 = new Vector<String>();
-		Vector<String> inChIKeys2 = new Vector<String>();
-		Vector<String> formulas = new Vector<String>();
-		Vector<String> names = new Vector<String>();
-		Vector<String> smiles = new Vector<String>();
-		Vector<Double> masses = new Vector<Double>();
-		Vector<Double> xlogps = new Vector<Double>();
-		try {
-			while(rs.next()) {
-				inchis.add(rs.getString(this.INCHI_COLUMN_NAME));
-				inChIKeys1.add(rs.getString(this.INCHIKEY1_COLUMN_NAME));
-				inChIKeys2.add(rs.getString(this.INCHIKEY2_COLUMN_NAME));
-				smiles.add(rs.getString(this.SMILES_COLUMN_NAME));
-				masses.add(rs.getDouble(this.MASS_COLUMN_NAME));
-				formulas.add(rs.getString(this.INCHI_COLUMN_NAME).split("/")[1]);
-				String name = "NA";
-				Double xlogp3 = null;
-				try {
-					xlogp3 = rs.getDouble("xlogp3_aa");
-				}
-				catch(org.postgresql.util.PSQLException e) {}
-				try {
-					xlogp3 = rs.getDouble("xlogp3");
-				}
-				catch(org.postgresql.util.PSQLException e) {}
-				try {
-					name = rs.getString(this.COMPOUND_NAME_COLUMN_NAME);
-				}
-				catch(org.postgresql.util.PSQLException e) {}
-				xlogps.add(xlogp3);
-				names.add(name);
-			}
-			rs.close();
-			this.statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		ICandidate candidate = new TopDownPrecursorCandidate(inchis.get(0), identifier);
-		candidate.setProperty(VariableNames.INCHI_KEY_1_NAME, inChIKeys1.get(0));
-		candidate.setProperty(VariableNames.INCHI_KEY_2_NAME, inChIKeys2.get(0));
-		candidate.setProperty(VariableNames.MOLECULAR_FORMULA_NAME, formulas.get(0));
-		candidate.setProperty(VariableNames.MONOISOTOPIC_MASS_NAME, masses.get(0));
-		candidate.setProperty(VariableNames.SMILES_NAME, smiles.get(0));
-		candidate.setProperty(VariableNames.COMPOUND_NAME_NAME, names.get(0));
-		if(xlogps.size() != 0) { 
-			if(xlogps.get(0) != null) candidate.setProperty("XlogP3", xlogps.get(0));
-			else candidate.setProperty("XlogP3", "NA");
-		}
-		return candidate;
-	}
-
-	/**
-	 * 
-	 */
-	public CandidateList getCandidateByIdentifier(Vector<String> identifiers) {
-		if(identifiers.size() == 0) return new CandidateList();
-
-		int library_id = this.getLibraryIdentifier();
-		if(library_id == -1) return new CandidateList();
-		String identifierString = identifiers.get(0) + "'";
-		for(String cid : identifiers)
-			identifierString += ",'" + cid + "'";
-		String query = "select substance." + this.CID_COLUMN_NAME + " as " + this.CID_COLUMN_NAME + ", name." + this.COMPOUND_NAME_COLUMN_NAME 
-				+ " as " + this.COMPOUND_NAME_COLUMN_NAME + ", compound." + this.SMILES_COLUMN_NAME + " as " + this.SMILES_COLUMN_NAME 
-				+ ", compound." + this.INCHI_COLUMN_NAME + " as " + this.INCHI_COLUMN_NAME + ", compound." 
-				+ this.MASS_COLUMN_NAME + " as " + this.MASS_COLUMN_NAME + " ," + " compound." + this.FORMULA_COLUMN_NAME 
-				+ " as " + this.FORMULA_COLUMN_NAME + ", compound." + this.INCHIKEY1_COLUMN_NAME + " as " + this.INCHIKEY1_COLUMN_NAME 
-				+ ", compound." + this.INCHIKEY2_COLUMN_NAME + " as " + this.INCHIKEY2_COLUMN_NAME 
-				+ ", substance.compound_id, substance.substance_id from substance, compound, name" 
-				+ " where substance.library_id='" + library_id + "'" 
-				+ " and substance." + this.CID_COLUMN_NAME + " in ('" + identifierString + ") "
-				+ " and substance.compound_id=compound.compound_id" 
-				+ " and name.substance_id=substance.substance_id"; 		
-		
-		ResultSet rs = this.submitQuery(query);
-		if(rs == null) return new CandidateList();
-		CandidateList candidates = new CandidateList();
+	
+	private void fillCandidateVectors(ResultSet rs) {
+		this.tempCandidates = new CandidateList();
+		this.tempSubstanceIDs = new Vector<String>();
 		try {
 			while(rs.next()) {
 				ICandidate candidate = new TopDownPrecursorCandidate(rs.getString(this.INCHI_COLUMN_NAME), rs.getString(this.CID_COLUMN_NAME));
@@ -231,15 +64,158 @@ public class LocalMetChemDatabase extends LocalPostgresDatabase {
 				try {
 					candidate.setProperty(VariableNames.COMPOUND_NAME_NAME, rs.getString(this.COMPOUND_NAME_COLUMN_NAME));
 				}
-				catch(org.postgresql.util.PSQLException e) {}
+				catch(org.postgresql.util.PSQLException e) {
+					
+				}
+				catch(java.lang.NullPointerException e) {
+					
+				}
 				
-				candidates.addElement(candidate);
+				this.tempSubstanceIDs.add(rs.getString("substance_id"));
+				this.tempCandidates.addElement(candidate);
 			}
 			rs.close();
 			this.statement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public Vector<String> getCandidateIdentifiers(double monoisotopicMass, double relativeMassDeviation) {
+		int library_id = this.getLibraryIdentifier();
+		if(library_id == -1) return null;
+		
+		double error = MathTools.calculateAbsoluteDeviation(monoisotopicMass, relativeMassDeviation);
+		/*	
+		String query = "select s.substance_id as substance_id, s." + this.CID_COLUMN_NAME + " as " + this.CID_COLUMN_NAME + ", n." + this.COMPOUND_NAME_COLUMN_NAME 
+				+ " as " + this.COMPOUND_NAME_COLUMN_NAME + ", c." + this.SMILES_COLUMN_NAME + " as " + this.SMILES_COLUMN_NAME 
+				+ ", c." + this.INCHI_COLUMN_NAME + " as " + this.INCHI_COLUMN_NAME + ", c." 
+				+ this.MASS_COLUMN_NAME + " as " + this.MASS_COLUMN_NAME + " ," + " c." + this.FORMULA_COLUMN_NAME 
+				+ " as " + this.FORMULA_COLUMN_NAME + ", c." + this.INCHIKEY1_COLUMN_NAME + " as " + this.INCHIKEY1_COLUMN_NAME 
+				+ ", c." + this.INCHIKEY2_COLUMN_NAME + " as " + this.INCHIKEY2_COLUMN_NAME + " " +
+				"from compound c inner join substance s on s.compound_id = c.compound_id " +
+				"inner join name n on n.substance_id = s.substance_id " +
+				"where c.monoisotopic_mass between " + (monoisotopicMass - error) + " and " + (monoisotopicMass + error) + " " +
+				"and s.library_id='" + library_id + "';";
+		*/
+		String query = "select n.name, s.substance_id, s." + this.CID_COLUMN_NAME + ", comp.compound_id , comp." + this.INCHI_COLUMN_NAME + ", comp." + this.MASS_COLUMN_NAME + ", comp." + this.FORMULA_COLUMN_NAME  + ", comp." + this.SMILES_COLUMN_NAME + ", comp." + this.INCHIKEY1_COLUMN_NAME + ", comp." + this.INCHIKEY2_COLUMN_NAME + " "
+				+ "from (select c.compound_id, c." + this.INCHI_COLUMN_NAME + ", c." + this.MASS_COLUMN_NAME + ", c." + this.FORMULA_COLUMN_NAME  + ", c." + this.SMILES_COLUMN_NAME + ", c." + this.INCHIKEY1_COLUMN_NAME + ", c." + this.INCHIKEY2_COLUMN_NAME  + " "
+				+ 	"from compound c where c." + this.MASS_COLUMN_NAME + " between '" + (monoisotopicMass - error) + "' and '" + (monoisotopicMass + error) + "') as comp "
+				+	"left join substance s on s.compound_id = comp.compound_id "
+				+	"left join name n on s.substance_id = n.substance_id where s.library_id='2';";
+		
+		ResultSet rs = this.submitQuery(query);
+		if(rs == null) return new Vector<String>();
+		this.fillCandidateVectors(rs);
+		return this.tempSubstanceIDs;
+	}
+	
+	public Vector<String> getCandidateIdentifiers(String molecularFormula) {
+		int library_id = this.getLibraryIdentifier();
+		if(library_id == -1) return null;
+		
+		/*
+		String query = "select s.substance_id as substance_id, s." + this.CID_COLUMN_NAME + " as " + this.CID_COLUMN_NAME + ", n." + this.COMPOUND_NAME_COLUMN_NAME 
+				+ " as " + this.COMPOUND_NAME_COLUMN_NAME + ", c." + this.SMILES_COLUMN_NAME + " as " + this.SMILES_COLUMN_NAME 
+				+ ", c." + this.INCHI_COLUMN_NAME + " as " + this.INCHI_COLUMN_NAME + ", c." 
+				+ this.MASS_COLUMN_NAME + " as " + this.MASS_COLUMN_NAME + " ," + " c." + this.FORMULA_COLUMN_NAME 
+				+ " as " + this.FORMULA_COLUMN_NAME + ", c." + this.INCHIKEY1_COLUMN_NAME + " as " + this.INCHIKEY1_COLUMN_NAME 
+				+ ", c." + this.INCHIKEY2_COLUMN_NAME + " as " + this.INCHIKEY2_COLUMN_NAME + " " +
+				"from compound c inner join substance s on s.compound_id = c.compound_id " +
+				"inner join name n on n.substance_id = s.substance_id " +
+				"where c.molecular_formula ='" + molecularFormula + "' " +
+				"and s.library_id='" + library_id + "';";
+		*/
+
+		String query = "select n.name, s.substance_id, s." + this.CID_COLUMN_NAME + ", comp.compound_id, comp." + this.INCHI_COLUMN_NAME + ", comp." + this.MASS_COLUMN_NAME + ", comp." + this.FORMULA_COLUMN_NAME  + ", comp." + this.SMILES_COLUMN_NAME + ", comp." + this.INCHIKEY1_COLUMN_NAME + ", comp." + this.INCHIKEY2_COLUMN_NAME + " "
+                                + "from (select c.compound_id, c." + this.INCHI_COLUMN_NAME + ", c." + this.MASS_COLUMN_NAME + ", c." + this.FORMULA_COLUMN_NAME  + ", c." + this.SMILES_COLUMN_NAME + ", c." + this.INCHIKEY1_COLUMN_NAME + ", c." + this.INCHIKEY2_COLUMN_NAME  + " "
+                                +       "from compound c where c." + this.FORMULA_COLUMN_NAME + " = '" + molecularFormula + "') as comp "
+                                +       "left join substance s on s.compound_id = comp.compound_id "
+                                +       "left join name n on s.substance_id = n.substance_id where s.library_id='2';";
+
+		ResultSet rs = this.submitQuery(query);
+		if(rs == null) return new Vector<String>();
+		this.fillCandidateVectors(rs);
+		return this.tempSubstanceIDs;
+	}
+
+	public Vector<String> getCandidateIdentifiers(Vector<String> identifiers) {
+		if(identifiers.size() == 0) return new Vector<String>();
+		
+		int library_id = this.getLibraryIdentifier();
+		if(library_id == -1) return null;
+		
+		String identifierString = "'" + identifiers.get(0) + "'";
+		for(String cid : identifiers)
+			identifierString += ",'" + cid + "'";
+		
+		String query = "select s.substance_id as substance_id, s." + this.CID_COLUMN_NAME + " as " + this.CID_COLUMN_NAME + ", n." + this.COMPOUND_NAME_COLUMN_NAME 
+				+ " as " + this.COMPOUND_NAME_COLUMN_NAME + ", c." + this.SMILES_COLUMN_NAME + " as " + this.SMILES_COLUMN_NAME 
+				+ ", c." + this.INCHI_COLUMN_NAME + " as " + this.INCHI_COLUMN_NAME + ", c." 
+				+ this.MASS_COLUMN_NAME + " as " + this.MASS_COLUMN_NAME + " ," + " c." + this.FORMULA_COLUMN_NAME 
+				+ " as " + this.FORMULA_COLUMN_NAME + ", c." + this.INCHIKEY1_COLUMN_NAME + " as " + this.INCHIKEY1_COLUMN_NAME 
+				+ ", c." + this.INCHIKEY2_COLUMN_NAME + " as " + this.INCHIKEY2_COLUMN_NAME + " " +
+				"from compound c inner join substance s on s.compound_id = c.compound_id " +
+				"inner join name n on n.substance_id = s.substance_id " +
+				"where s." + this.CID_COLUMN_NAME + " in (" + identifierString + ") " +
+				"and s.library_id='" + library_id + "';";
+
+		ResultSet rs = this.submitQuery(query);
+		if(rs == null) return new Vector<String>();
+		this.fillCandidateVectors(rs);
+		return this.tempSubstanceIDs;
+	}
+
+	public Vector<String> getCandidateIdentifiers(String[] identifiers) {
+		
+		if(identifiers == null || identifiers.length == 0) return new Vector<String>();
+		Vector<String> ids = new Vector<String>();
+		for(String id : identifiers)
+			ids.add(id);
+		return this.getCandidateIdentifiers(ids);
+	}
+	
+	/**
+	 * identifier is a MetChem internal identifier namely a substance_id
+	 */
+	public ICandidate getCandidateByIdentifier(String identifier) {
+		if(this.tempCandidates == null || this.tempSubstanceIDs == null || this.tempCandidates.getNumberElements() == 0) return null;
+		
+		for(int i = 0; i < this.tempSubstanceIDs.size(); i++) {
+			if(this.tempSubstanceIDs.get(i).equals(identifier)) {
+				ICandidate candidate = this.tempCandidates.getElement(i);
+				this.tempCandidates = null;
+				this.tempSubstanceIDs = null;
+				return candidate; 
+			}
+		}
+		
+		this.tempCandidates = null;
+		this.tempSubstanceIDs = null;
+		
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	public CandidateList getCandidateByIdentifier(Vector<String> identifiers) {
+		if(identifiers.size() == 0 || this.tempSubstanceIDs == null || this.tempCandidates == null || this.tempCandidates.getNumberElements() == 0) 
+			return new CandidateList();
+		
+		CandidateList candidates = new CandidateList();
+		for(int j = 0; j < identifiers.size(); j++) {
+			for(int i = 0; i < this.tempSubstanceIDs.size(); i++) {
+				if(this.tempSubstanceIDs.get(i).equals(identifiers.get(j))) {
+					candidates.addElement(this.tempCandidates.getElement(i));
+					break;
+				}
+			}
+		}	
+		
+		this.tempCandidates = null;
+		this.tempSubstanceIDs = null;
+		
 		return candidates;
 	}
 
