8cc66c420bc99ede02bbd4bfe9fa6bab723aedd9
Christoph Ruttkies <christoph.ruttkies@ipb-halle.de>
2017-01-30 23:37:16 +0100

    updated HDX simulation and spectra comparison

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/tools/SimulateSpectrumHDX.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/tools/SimulateSpectrumHDX.java
@@ -18,32 +18,53 @@ import de.ipbhalle.metfraglib.settings.MetFragGlobalSettings;
 import de.ipbhalle.metfraglib.fragment.DefaultBitArrayFragment;
 import de.ipbhalle.metfraglib.match.DefaultFragmentToPeakMatch;
 
+/**
+ * generate a simulated spectrum given a candidate and its native spectrum
+ * 
+ * input: 
+ * 
+ * parameter file: MetFrag parameter file 
+ * output file: where to write output
+ * method: 
+ * 	1 - [M+D]+
+ * 	2 - [M]+
+ * 	3 - [M]+ & [M+D]+
+ * 	4 - predict masses based on in silico added deuteriums
+ * 	5 - predict masses based adduct type [F+H]+ -> [F+D]+
+ * 
+ * charge: pos or neg
+ * 
+ * @author chrisr
+ *
+ */
 public class SimulateSpectrumHDX {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		MetFragGlobalSettings settings = MetFragGlobalSettings.readSettings(new File(args[0]), null);
 		String outputfile = args[1];
 		int method = Integer.parseInt(args[2]);
 		String type = args[3];
-		
+
 		CombinedMetFragProcess metfragProcess = new CombinedMetFragProcess(settings);
-		
+
 		try {
 			metfragProcess.retrieveCompounds();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		metfragProcess.run();
-		
-		ScoredCandidateList scoredCandidateList = (ScoredCandidateList)metfragProcess.getCandidateList();
-		
+
+		ScoredCandidateList scoredCandidateList = (ScoredCandidateList) metfragProcess.getCandidateList();
+
 		try {
 			double[][] spectrum = null;
-			if(type.equals("pos")) spectrum = generateDeuteratedSpectrumPositive(scoredCandidateList.getElement(0), method);
-			else if(type.equals("neg")) spectrum = generateDeuteratedSpectrumNegative(scoredCandidateList.getElement(0), method);
+			if (type.equals("pos"))
+				spectrum = generateDeuteratedSpectrumPositive(scoredCandidateList.getElement(0), method);
+			else if (type.equals("neg"))
+				spectrum = generateDeuteratedSpectrumNegative(scoredCandidateList.getElement(0), method);
 			BufferedWriter bwriter = new BufferedWriter(new FileWriter(new File(outputfile)));
-			for(int i = 0; i < spectrum.length; i++) {
+			for (int i = 0; i < spectrum.length; i++) {
 				bwriter.write(spectrum[i][0] + " " + spectrum[i][1]);
 				bwriter.newLine();
 			}
@@ -55,9 +76,9 @@ public class SimulateSpectrumHDX {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @param candidate
@@ -65,54 +86,68 @@ public class SimulateSpectrumHDX {
 	 * @throws AtomTypeNotKnownFromInputListException
 	 * @throws Exception
 	 */
-	public static double[][] generateDeuteratedSpectrumPositive(ICandidate candidate, int method) throws AtomTypeNotKnownFromInputListException, Exception {
-		int numberPositions = searchForDeuteriumExchangeablePositions((TopDownBitArrayPrecursor)candidate.getPrecursorMolecule()).length;
-		HDTopDownBitArrayPrecursor preHDX = new HDTopDownBitArrayPrecursor(candidate.getImplicitHydrogenAtomContainer(), (byte)numberPositions);
+	public static double[][] generateDeuteratedSpectrumPositive(ICandidate candidate, int method)
+			throws AtomTypeNotKnownFromInputListException, Exception {
+		int numberPositions = searchForDeuteriumExchangeablePositions(
+				(TopDownBitArrayPrecursor) candidate.getPrecursorMolecule()).length;
+		HDTopDownBitArrayPrecursor preHDX = new HDTopDownBitArrayPrecursor(candidate.getImplicitHydrogenAtomContainer(),
+				(byte) numberPositions);
 		preHDX.preprocessPrecursor();
 		MatchList matchList = candidate.getMatchList();
 		double[][] spectrumHDX = null;
-		if(method == 3) spectrumHDX = new double[matchList.getNumberElements() * 2][2];
-		else spectrumHDX = new double[matchList.getNumberElements()][2];
-		for(int i = 0; i < matchList.getNumberElements(); i++) {
+		if (method == 3)
+			spectrumHDX = new double[matchList.getNumberElements() * 2][2];
+		else
+			spectrumHDX = new double[matchList.getNumberElements()][2];
+		for (int i = 0; i < matchList.getNumberElements(); i++) {
 			IFragment frag = matchList.getElement(i).getBestMatchedFragment();
 			IPeak peak = matchList.getElement(i).getMatchedPeak();
-			int[] setAtoms = ((DefaultBitArrayFragment)frag).getAtomsBitArray().getSetIndeces();
+			int[] setAtoms = ((DefaultBitArrayFragment) frag).getAtomsBitArray().getSetIndeces();
 			int numberDeuteriums = 0;
-			for(int k = 0; k < setAtoms.length; k++) {
+			for (int k = 0; k < setAtoms.length; k++) {
 				numberDeuteriums += preHDX.getNumberDeuteriumsConnectedToAtomIndex(0, setAtoms[k]);
 			}
 			spectrumHDX[i][1] = peak.getAbsoluteIntensity();
 			// [M+D]+
-			if(method == 1) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
-                        }
-                        // [M]+
-                        else if(method == 2) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            // nothing to do
-                        }
-                        // [M]+ [M+D]+
-                        else if(method == 3) {
-                            spectrumHDX[i * 2][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            spectrumHDX[(i * 2) + 1][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D"); 
-                            spectrumHDX[(i * 2) + 1][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
-                        }
-                        // predict based on deuteriums
-                        else if(method == 4) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            if(numberDeuteriums != 0) spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
-                        }
-                        // predict based on adduct
-                        else if(method == 5) {
-                            byte adductTypeIndex = ((DefaultFragmentToPeakMatch)matchList.getElement(i)).getBestMatchedFragmentAdductTypeIndex();
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            if(adductTypeIndex == 2) spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
-                        }
+			if (method == 1) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
+			}
+			// [M]+
+			else if (method == 2) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				// nothing to do
+			}
+			// [M]+ [M+D]+
+			else if (method == 3) {
+				spectrumHDX[i * 2][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[(i * 2) + 1][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[(i * 2) + 1][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
+			}
+			// predict based on in silico added deuteriums
+			else if (method == 4) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				if (numberDeuteriums != 0)
+					spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
+			}
+			// predict based on adduct
+			else if (method == 5) {
+				byte adductTypeIndex = ((DefaultFragmentToPeakMatch) matchList.getElement(i))
+						.getBestMatchedFragmentAdductTypeIndex();
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				if (adductTypeIndex == 2)
+					spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
+			}
 		}
 		return spectrumHDX;
 	}
-	
+
 	/**
 	 * 
 	 * @param candidate
@@ -120,54 +155,68 @@ public class SimulateSpectrumHDX {
 	 * @throws AtomTypeNotKnownFromInputListException
 	 * @throws Exception
 	 */
-	public static double[][] generateDeuteratedSpectrumNegative(ICandidate candidate, int method) throws AtomTypeNotKnownFromInputListException, Exception {
-		int numberPositions = searchForDeuteriumExchangeablePositions((TopDownBitArrayPrecursor)candidate.getPrecursorMolecule()).length;
-		HDTopDownBitArrayPrecursor preHDX = new HDTopDownBitArrayPrecursor(candidate.getImplicitHydrogenAtomContainer(), (byte)numberPositions);
+	public static double[][] generateDeuteratedSpectrumNegative(ICandidate candidate, int method)
+			throws AtomTypeNotKnownFromInputListException, Exception {
+		int numberPositions = searchForDeuteriumExchangeablePositions(
+				(TopDownBitArrayPrecursor) candidate.getPrecursorMolecule()).length;
+		HDTopDownBitArrayPrecursor preHDX = new HDTopDownBitArrayPrecursor(candidate.getImplicitHydrogenAtomContainer(),
+				(byte) numberPositions);
 		preHDX.preprocessPrecursor();
 		MatchList matchList = candidate.getMatchList();
 		double[][] spectrumHDX = null;
-		if(method == 3) spectrumHDX = new double[matchList.getNumberElements() * 2][2];
-		else spectrumHDX = new double[matchList.getNumberElements()][2];
-		for(int i = 0; i < matchList.getNumberElements(); i++) {
+		if (method == 3)
+			spectrumHDX = new double[matchList.getNumberElements() * 2][2];
+		else
+			spectrumHDX = new double[matchList.getNumberElements()][2];
+		for (int i = 0; i < matchList.getNumberElements(); i++) {
 			IFragment frag = matchList.getElement(i).getBestMatchedFragment();
 			IPeak peak = matchList.getElement(i).getMatchedPeak();
-			int[] setAtoms = ((DefaultBitArrayFragment)frag).getAtomsBitArray().getSetIndeces();
+			int[] setAtoms = ((DefaultBitArrayFragment) frag).getAtomsBitArray().getSetIndeces();
 			int numberDeuteriums = 0;
-			for(int k = 0; k < setAtoms.length; k++) {
+			for (int k = 0; k < setAtoms.length; k++) {
 				numberDeuteriums += preHDX.getNumberDeuteriumsConnectedToAtomIndex(0, setAtoms[k]);
 			}
 			spectrumHDX[i][1] = peak.getAbsoluteIntensity();
 			// [M-D]-
-			if(method == 1) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            spectrumHDX[i][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
-                        }
-                        // [M]-
-                        else if(method == 2) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            // nothing to do
-                        }
-                        // [M]- [M-D]-
-                        else if(method == 3) {
-                            spectrumHDX[i * 2][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            spectrumHDX[(i * 2) + 1][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D"); 
-                            spectrumHDX[(i * 2) + 1][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
-                        }
-                        // predict based on deuteriums
-                        else if(method == 4) {
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            if(numberDeuteriums != 0) spectrumHDX[i][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
-                        }
-                        // predict based on adduct
-                        else if(method == 5) {
-                            byte adductTypeIndex = ((DefaultFragmentToPeakMatch)matchList.getElement(i)).getBestMatchedFragmentAdductTypeIndex();
-                            spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS + (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
-                            if(adductTypeIndex == 1) spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
-                        }
+			if (method == 1) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[i][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
+			}
+			// [M]-
+			else if (method == 2) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				// nothing to do
+			}
+			// [M]- [M-D]-
+			else if (method == 3) {
+				spectrumHDX[i * 2][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[(i * 2) + 1][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				spectrumHDX[(i * 2) + 1][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
+			}
+			// predict based on deuteriums
+			else if (method == 4) {
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				if (numberDeuteriums != 0)
+					spectrumHDX[i][0] += Constants.HYDROGEN_MASS - Constants.getMonoisotopicMassOfAtom("D");
+			}
+			// predict based on adduct
+			else if (method == 5) {
+				byte adductTypeIndex = ((DefaultFragmentToPeakMatch) matchList.getElement(i))
+						.getBestMatchedFragmentAdductTypeIndex();
+				spectrumHDX[i][0] = peak.getMass() - (numberDeuteriums) * Constants.HYDROGEN_MASS
+						+ (numberDeuteriums) * Constants.getMonoisotopicMassOfAtom("D");
+				if (adductTypeIndex == 1)
+					spectrumHDX[i][0] += Constants.getMonoisotopicMassOfAtom("D") - Constants.HYDROGEN_MASS;
+			}
 		}
 		return spectrumHDX;
 	}
-	
+
 	/**
 	 * 
 	 * @param con
@@ -182,7 +231,7 @@ public class SimulateSpectrumHDX {
 				continue;
 			for (int k = 0; k < elementsToExchange.length; k++) {
 				if (symbol.equals(elementsToExchange[k]) && con.getNumberHydrogensConnectedToAtomIndex(i) > 0) {
-					for(int l = 0; l < con.getNumberHydrogensConnectedToAtomIndex(i); l++) {
+					for (int l = 0; l < con.getNumberHydrogensConnectedToAtomIndex(i); l++) {
 						positionsToExchange.add(i);
 					}
 					break;
@@ -190,11 +239,11 @@ public class SimulateSpectrumHDX {
 			}
 		}
 		int[] array = new int[positionsToExchange.size()];
-		for(int i = 0; i < positionsToExchange.size(); i++) {
+		for (int i = 0; i < positionsToExchange.size(); i++) {
 			array[i] = positionsToExchange.get(i);
 		}
-		
+
 		return array;
 	}
-	
+
 }
