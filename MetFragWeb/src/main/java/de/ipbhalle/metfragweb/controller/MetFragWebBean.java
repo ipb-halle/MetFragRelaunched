89ae4ab4c3ffdb301032d97eafd7137382618deb
Christoph Ruttkies <christoph.ruttkies@ipb-halle.de>
2017-04-29 23:36:54 +0200

    adapted version of MetFragWeb

--- MetFragWeb/src/main/java/de/ipbhalle/metfragweb/controller/MetFragWebBean.java
+++ MetFragWeb/src/main/java/de/ipbhalle/metfragweb/controller/MetFragWebBean.java
@@ -72,7 +72,7 @@ import de.ipbhalle.metfragweb.validator.SmartsValidator;
 @SessionScoped
 public class MetFragWebBean {
 
-	private final String version = "v2.0.2";
+	private final String version = "v2.0.3";
 	/*
 	 * combines all the settings
 	 */
