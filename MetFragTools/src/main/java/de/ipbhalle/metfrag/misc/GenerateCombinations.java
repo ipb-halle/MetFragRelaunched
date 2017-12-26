e9e2f2c23c5a2dc019d1e23abe4a04f6516d104e
Christoph Ruttkies <christoph.ruttkies@ipb-halle.de>
2017-12-26 01:43:55 +0100

    updates

--- MetFragTools/src/main/java/de/ipbhalle/metfrag/misc/GenerateCombinations.java
+++ MetFragTools/src/main/java/de/ipbhalle/metfrag/misc/GenerateCombinations.java
@@ -6,6 +6,21 @@ public class GenerateCombinations {
 
 	public static void main(String[] args) throws IOException {
 		
+		String[] a1 = {"0.5","0.25","0.125","0.0625","0.0125","0.0025","5e-04","1e-04","2e-05","4e-06","8e-07","1.6e-07","3.2e-08"};
+		String[] b1 = {"0.5","0.25","0.125","0.0625","0.0125","0.0025","5e-04","1e-04","2e-05","4e-06","8e-07","1.6e-07","3.2e-08"};
+		
+		java.io.BufferedWriter bwriter = new java.io.BufferedWriter(new java.io.FileWriter(new java.io.File("/tmp/pseudocounts_slim.txt")));
+		for(int i1 = 0; i1 < a1.length; i1++) {
+			for(int i2 = 0; i2 < b1.length; i2++) {
+				bwriter.write(a1[i1] + " " + b1[i2]);
+				bwriter.newLine();
+			}
+		}
+		bwriter.close();
+	}
+	/*
+	public static void main(String[] args) throws IOException {
+		
 		String[] a1 = {"1e-04", "5e-04", "0.0025"};
 		String[] b1 = {"1e-04", "5e-04", "0.0025"};
 		String[] a2 = {"1e-04", "5e-04", "0.0025"};
@@ -24,6 +39,6 @@ public class GenerateCombinations {
 		}
 		bwriter.close();
 	}
-	
+	*/
 }
 
