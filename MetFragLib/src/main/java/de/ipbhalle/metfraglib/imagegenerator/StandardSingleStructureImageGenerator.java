cef3cd396aae42e7a5519c8c12ebb6562d9a5b12
Ruttkies <cruttkie@ipb-halle.de>
2018-04-16 17:01:15 +0200

    updates

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/imagegenerator/StandardSingleStructureImageGenerator.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/imagegenerator/StandardSingleStructureImageGenerator.java
@@ -201,7 +201,7 @@ public class StandardSingleStructureImageGenerator implements IImageGenerator {
 		 IAtomContainer m = null;
 			try {
 				//m = MoleculeFunctions.getAtomContainerFromInChI("InChI=1S/C15H14O6/c16-8-4-11(18)9-6-13(20)15(21-14(9)5-8)7-1-2-10(17)12(19)3-7/h1-5,13,15-20H,6H2/t13-,15-/m1/s1");
-				m = MoleculeFunctions.getAtomContainerFromSMILES("CC1=CN2CC(CN=C2C=C1)O");
+				m = MoleculeFunctions.getAtomContainerFromSMILES("C1C(OC2=C(C1=O)C(=C(C=C2)O)O)C3=CC=CC=C3O");
 
 				MoleculeFunctions.prepareAtomContainer(m, false);
 				//MoleculeFunctions.removeHydrogens(m);
