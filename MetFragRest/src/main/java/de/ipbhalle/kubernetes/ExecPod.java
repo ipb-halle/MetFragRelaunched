dc6b80eda02c6009acfeb0288b06d2b6cb9ff594
Ruttkies <cruttkie@ipb-halle.de>
2018-04-16 17:01:15 +0200

    updates

--- MetFragRest/src/main/java/de/ipbhalle/kubernetes/ExecPod.java
+++ MetFragRest/src/main/java/de/ipbhalle/kubernetes/ExecPod.java
@@ -1,5 +1,8 @@
 package de.ipbhalle.kubernetes;
 
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+
 public class ExecPod {
 	public static void main(String[] args) throws InterruptedException {
         String master = "https://localhost:8443/";
