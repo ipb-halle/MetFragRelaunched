d9a328464c43fc9a898558b2e1b8e3b1ff9ead84
René Meier <rmeier@ipb-halle.de>
2022-08-01 16:42:33 +0200

    Start multi reference library loader code

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/peaklistreader/MultipleTandemMassPeakListReader.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/peaklistreader/MultipleTandemMassPeakListReader.java
@@ -2,6 +2,9 @@ package de.ipbhalle.metfraglib.peaklistreader;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import de.ipbhalle.metfraglib.additionals.MoleculeFunctions;
 import de.ipbhalle.metfraglib.collection.SpectralPeakListCollection;
@@ -17,6 +20,74 @@ public class MultipleTandemMassPeakListReader extends AbstractPeakListReader {
 	
 	public MultipleTandemMassPeakListReader(Settings settings) {
 		super(settings);
+	}
+	
+	public SpectralPeakListCollection readMultiple(List<Path> libFiles) throws Exception {
+		SpectralPeakListCollection spectralPeakListCollection = 
+				new SpectralPeakListCollection(
+						(Boolean)this.settings.get(VariableNames.IS_POSITIVE_ION_MODE_NAME),
+						(Double)this.settings.get(VariableNames.ABSOLUTE_MASS_DEVIATION_NAME),
+						(Double)this.settings.get(VariableNames.RELATIVE_MASS_DEVIATION_NAME),
+						(Double)this.settings.get(VariableNames.MINIMUM_COSINE_SIMILARITY_LIMIT_NAME));
+		SortedSimilarityTandemMassPeakList peakList = null;
+		int precursorIonTypeIndex = 0;
+		
+		for (Path libfile:libFiles) {
+			java.io.BufferedReader breader = new java.io.BufferedReader(new java.io.InputStreamReader(Files.newInputStream(libfile)));
+			String line = "";
+			while((line = breader.readLine()) != null) {
+				line = line.trim();
+				if(line.length() == 0 && peakList != null) {
+					if(peakList.isFullyInitialised()) {
+						peakList.calculateRelativeIntensities(Constants.DEFAULT_MAXIMUM_RELATIVE_INTENSITY);
+						spectralPeakListCollection.addPeakList(peakList);
+					}
+					peakList = null;
+				}
+				else if(line.length() == 0) continue;
+				else if(line.startsWith("#")) { //comment
+					if(peakList == null) peakList = new SortedSimilarityTandemMassPeakList(0.0);
+					line = line.replaceFirst("^#\\s*", "");
+					String[] tmp = line.split("=");
+					String paramname = tmp[0].trim();
+					String valuename = tmp.length >= 1 ? tmp[1] : "";
+					for(int i = 2; i < tmp.length; i++) {
+						valuename += "=" + tmp[i];
+					}
+					valuename = valuename.trim();
+					try {
+						if(paramname.equals(VariableNames.PRECURSOR_ION_MASS_NAME)) peakList.setMeasuredPrecursorMass(Double.parseDouble(valuename));
+						if(paramname.equals(VariableNames.PRECURSOR_ION_MODE_NAME)) precursorIonTypeIndex = Constants.ADDUCT_NOMINAL_MASSES.indexOf(Integer.parseInt(valuename));
+						if(paramname.equals(VariableNames.INCHI_NAME)) peakList.setInchi(valuename);
+						if(paramname.equals(VariableNames.INCHI_KEY_NAME)) peakList.setInchikey1(valuename.split("-")[0]);
+						if(paramname.equals(VariableNames.IS_POSITIVE_ION_MODE_NAME)) peakList.setIsPositiveCharge(valuename.toLowerCase().equals("true"));
+						if(paramname.equals(VariableNames.SAMPLE_NAME)) peakList.setSampleName(valuename);
+						if(paramname.equals(VariableNames.MOLECULAR_FINGERPRINT_NAME)) peakList.setFingerprint(MoleculeFunctions.stringToFingerPrint(valuename));
+					}
+					catch(Exception e) {
+						System.err.println("Error with spectrum " + peakList.getSampleName());
+						e.printStackTrace();
+						System.exit(1);
+					}
+				}
+				else { //peak
+					String[] tmp = line.split("\\s+");
+					double currentMass = Double.parseDouble(tmp[0].trim());
+					double currentIntensity = Double.parseDouble(tmp[1].trim());
+					if(currentMass >= peakList.getMeasuredPrecursorMass() - 5.0 + Constants.ADDUCT_MASSES.get(precursorIonTypeIndex))
+						continue;
+					peakList.addElement(new TandemMassPeak(currentMass, currentIntensity));
+				}
+			}
+			breader.close();
+		}
+		
+		if(peakList != null) {
+			peakList.calculateRelativeIntensities(Constants.DEFAULT_MAXIMUM_RELATIVE_INTENSITY);
+			spectralPeakListCollection.addPeakList(peakList);
+		}
+		
+		return spectralPeakListCollection;
 	}
 	
 	public SpectralPeakListCollection readMultiple(java.io.InputStream stream) throws Exception {
