a81be39e71a428d00b01e80677958fba381b5974
Rene Meier <meier.rene@googlemail.com>
2024-10-25 12:22:22 +0200

    Load default reference data from resource directory if it is a directory or from file if it is a file.

--- MetFragLib/src/main/java/de/ipbhalle/metfraglib/scoreinitialisation/OfflineMetFusionSpectralSimilarityScoreInitialiser.java
+++ MetFragLib/src/main/java/de/ipbhalle/metfraglib/scoreinitialisation/OfflineMetFusionSpectralSimilarityScoreInitialiser.java
@@ -1,13 +1,14 @@
 package de.ipbhalle.metfraglib.scoreinitialisation;
 
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import de.ipbhalle.metfraglib.collection.SpectralPeakListCollection;
@@ -23,42 +24,53 @@ public class OfflineMetFusionSpectralSimilarityScoreInitialiser implements IScor
 
 	@Override
 	public void initScoreParameters(Settings settings) throws Exception {
-		if(!settings.containsKey(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) || settings.get(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) == null) {
-			if(settings.containsKey(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) && settings.get(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) != null) return;
-			
-			MultipleTandemMassPeakListReader multiplePeakListReader = new MultipleTandemMassPeakListReader(settings);
-			SpectralPeakListCollection spectralPeakLists = null;
-			Path offlineSpectralFilePath = null; 
-			if(settings.containsKey(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) && settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) != null) {
-				offlineSpectralFilePath = Paths.get((String)settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME));
-			}
-			
-			if (offlineSpectralFilePath !=null && Files.isRegularFile(offlineSpectralFilePath)) {
-				logger.info("Load reference data from file: " +offlineSpectralFilePath );
-				List<Path> libFiles = new ArrayList<Path>();
-				libFiles.add(offlineSpectralFilePath);
-				InputStream inStream = Files.newInputStream(offlineSpectralFilePath);
-				spectralPeakLists = multiplePeakListReader.readMultiple(inStream);
-			} else if (offlineSpectralFilePath !=null && Files.isDirectory(offlineSpectralFilePath)) {
-				logger.info("Load reference data from directory: " +offlineSpectralFilePath );
-				List<Path> libFiles = Files.walk(offlineSpectralFilePath)
-						.filter(Files::isRegularFile)
-						.filter(p -> p.getFileName().toString().endsWith(".mb"))
-						.collect(Collectors.toList());
-				spectralPeakLists = multiplePeakListReader.readMultiple(libFiles);
-			}
-			else {
-				logger.info("Load reference data from resource file \"MoNA-export-LC-MS.mb\".");
-				InputStream inStream = null;
-				inStream = OfflineMetFusionSpectralSimilarityScoreInitialiser.class.getResourceAsStream("/MoNA-export-LC-MS.mb");
-				spectralPeakLists = multiplePeakListReader.readMultiple(inStream);
-				inStream.close();
-			}
-			//check whether MoNA InChIKeys are given as resource
-			spectralPeakLists.calculateSimilarities((SortedTandemMassPeakList)settings.get(VariableNames.PEAK_LIST_NAME));
-			
-			settings.set(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME, spectralPeakLists);
+		logger.setLevel(Level.toLevel("info"));
+
+		if (settings.containsKey(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) &&
+				settings.get(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME) != null) {
+			return;
 		}
+
+		MultipleTandemMassPeakListReader multiplePeakListReader = new MultipleTandemMassPeakListReader(settings);
+		SpectralPeakListCollection spectralPeakLists = null;
+		Path offlineSpectralFilePath = null;
+
+		if (settings.containsKey(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) &&
+            settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME) != null) {
+            offlineSpectralFilePath = Paths.get((String) settings.get(VariableNames.OFFLINE_SPECTRAL_DATABASE_FILE_NAME));
+        } else {
+			logger.info("Load reference data from default resource \"MoNA-export-LC-MS.mb\".");
+			URI uri = Objects.requireNonNull(getClass().getClassLoader().getResource("/MoNA-export-LC-MS.mb")).toURI();
+			if ("jar".equals(uri.getScheme())) {
+				try {
+					FileSystems.getFileSystem(uri);
+				} catch (FileSystemNotFoundException e) {
+					FileSystems.newFileSystem(uri, Collections.emptyMap());
+				}
+			}
+			offlineSpectralFilePath = Paths.get(uri);
+		}
+
+		if (Files.isRegularFile(offlineSpectralFilePath)) {
+			logger.info("Load reference data from file: " + offlineSpectralFilePath);
+			try (InputStream inStream = Files.newInputStream(offlineSpectralFilePath)) {
+				spectralPeakLists = multiplePeakListReader.readMultiple(inStream);
+			}
+		} else if (Files.isDirectory(offlineSpectralFilePath)) {
+			logger.info("Load reference data from directory: " + offlineSpectralFilePath);
+			List<Path> libFiles = Files.walk(offlineSpectralFilePath)
+					.filter(Files::isRegularFile)
+					.filter(p -> p.getFileName().toString().endsWith(".mb"))
+					.collect(Collectors.toList());
+			logger.info("Reference data files found: " + libFiles);
+			spectralPeakLists = multiplePeakListReader.readMultiple(libFiles);
+		}
+
+		if (spectralPeakLists != null) {
+			//check whether MoNA InChIKeys are given as resource
+            spectralPeakLists.calculateSimilarities((SortedTandemMassPeakList) settings.get(VariableNames.PEAK_LIST_NAME));
+            settings.set(VariableNames.OFFLINE_METFUSION_MONA_SPECTRAL_SIMILARITY_PEAK_LIST_COLLECTION_NAME, spectralPeakLists);
+        }
 	}
 
 	public void postProcessScoreParameters(Settings settings) {
